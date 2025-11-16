2ca0273211b7460f9651ea245560eb968d46b7f2
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:44:11 +0530

    Update main.go

--- 3. Write a GO program to use struct that is/main.go
+++ 3. Write a GO program to use struct that is/main.go
@@ -1,11 +1,10 @@
-package main
-
-import (
-	"fmt"
-	"myproject/week-3/newpack" // use your module name here
-)
-
-func main() {
-	num := newpack.Number{Value: 5}
-	fmt.Println("Square of 5 is:", num.Square())
-}
+package main 
+import ( 
+    "fmt" 
+    "newpack" 
+) 
+func main() { 
+    num := newpack.Number{Value: 5}
+    result := num.Square()         
+    fmt.Println("Square of 5 is:", result) 
+} 
