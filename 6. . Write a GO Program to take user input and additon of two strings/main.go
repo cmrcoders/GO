a8b91cb51ba5b361ac48880acce0e1aeff72cdc2
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:47:52 +0530

    Update main.go

--- 6. . Write a GO Program to take user input and additon of two strings/main.go
+++ 6. . Write a GO Program to take user input and additon of two strings/main.go
@@ -1,14 +1,12 @@
 // 6. Write a GO Program to take user input and addition of two strings.
-package main
-
-import "fmt"
-
-func main() {
-	var first, second string
-	fmt.Print("Enter First String: ")
-	fmt.Scanln(&first)
-	fmt.Print("Enter Second String: ")
-	fmt.Scanln(&second)
-	result := first + " " + second
-	fmt.Println("Concatenation of two strings:", result)
-}
+package main 
+import "fmt" 
+func main() { 
+    var first, second string 
+    fmt.Print("Enter First String: ") 
+    fmt.Scanln(&first) 
+    fmt.Print("Enter Second String: ") 
+    fmt.Scanln(&second) 
+    result := first + second 
+    fmt.Println("Concatenation of two strings:", result) 
+} 
