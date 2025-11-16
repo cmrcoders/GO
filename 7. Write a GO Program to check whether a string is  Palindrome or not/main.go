f5db405b527a7d167381ce8e638c4704d440ae8f
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:48:45 +0530

    Update main.go

--- 7. Write a GO Program to check whether a string is  Palindrome or not/main.go
+++ 7. Write a GO Program to check whether a string is  Palindrome or not/main.go
@@ -1,19 +1,18 @@
 // 7. Write a GO Program to check whether a string is Palindrome or not.
-package main
-
-import "fmt"
-
-func main() {
-	var str string
-	fmt.Print("Enter a string: ")
-	fmt.Scanln(&str)
-	rev := ""
-	for i := len(str) - 1; i >= 0; i-- {
-		rev += string(str[i])
-	}
-	if str == rev {
-		fmt.Printf("The string '%s' is a Palindrome.\n", str)
-	} else {
-		fmt.Printf("The string '%s' is not a Palindrome.\n", str)
-	}
-}
+package main 
+import "fmt" 
+func main() { 
+    var str string 
+    fmt.Print("Enter a string: ") 
+    fmt.Scanln(&str) 
+    original := str 
+    reversed := "" 
+    for i := len(str) - 1; i >= 0; i-- { 
+        reversed += string(str[i]) 
+    } 
+    if original == reversed { 
+        fmt.Printf("\"%s\" is a Palindrome\n", original) 
+    } else { 
+        fmt.Printf("\"%s\" is NOT a Palindrome\n", original) 
+    } 
+} 
