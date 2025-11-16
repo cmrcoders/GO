af2af1bedaf28d4cb7cc47d734dfbce990eb528f
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:43:06 +0530

    Update main.go

--- 2. Write a GO Program to print Pyramid of numbers/main.go
+++ 2. Write a GO Program to print Pyramid of numbers/main.go
@@ -1,22 +1,20 @@
 // 2. Write a GO Program to print Pyramid of numbers.
 package main
-
-import "fmt"
-
-func main() {
-	var n int
-	fmt.Print("Enter number of levels: ")
-	fmt.Scan(&n)
-	for i := 1; i <= n; i++ {
-		for s := 1; s <= n-i; s++ {
-			fmt.Print(" ")
-		}
-		for j := 1; j <= i; j++ {
-			fmt.Print(j)
-		}
-		for j := i - 1; j >= 1; j-- {
-			fmt.Print(j)
-		}
-		fmt.Println()
-	}
-}
+import "fmt" 
+func main() { 
+    var n int 
+    fmt.Print("Enter number of levels: ") 
+    fmt.Scan(&n) 
+    for i := 1; i <= n; i++ { 
+        for j := 1; j <= n-i; j++ { 
+            fmt.Print(" ") 
+        } 
+        for j := 1; j <= i; j++ { 
+            fmt.Print(j) 
+        } 
+        for j := i - 1; j >= 1; j-- { 
+            fmt.Print(j) 
+        } 
+        fmt.Println() 
+    } 
+} 
