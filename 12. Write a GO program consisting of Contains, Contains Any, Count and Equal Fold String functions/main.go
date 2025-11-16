de3e6694aa316aa1d46144f066be86e203e69722
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:41:58 +0530

    Update main.go

--- 12. Write a GO program consisting of Contains, Contains Any, Count and Equal Fold String functions/main.go
+++ 12. Write a GO program consisting of Contains, Contains Any, Count and Equal Fold String functions/main.go
@@ -1,18 +1,23 @@
 // 12. Write a GO program consisting of Contains, Contains Any, Count and Equal Fold String functions.
-package main
-
-import (
-	"fmt"
-	"strings"
-)
-
-func main() {
-	fmt.Println(strings.Contains("HelloWorld", "World")) // true
-	fmt.Println(strings.Contains("HelloWorld", "world")) // false
-	fmt.Println(strings.ContainsAny("Hello", "aeiou"))   // true
-	fmt.Println(strings.ContainsAny("Sky", "aeiou"))     // false
-	fmt.Println(strings.Count("cheese", "e"))            // 3 (true means >0)
-	fmt.Println(strings.Count("cheese", "z"))            // 0 (false means none found)
-	fmt.Println(strings.EqualFold("GoLang", "golang"))   // true
-	fmt.Println(strings.EqualFold("Hello", "World"))     // false
-}
+package main 
+import ( 
+    "fmt" 
+    "strings" 
+) 
+func main() { 
+    fmt.Println("ContainsAny Examples:") 
+    fmt.Println(strings.ContainsAny("Germany", "G"))   // true 
+    fmt.Println(strings.ContainsAny("Germany", "g"))   // false 
+    fmt.Println() 
+    fmt.Println("Contains Examples:") 
+    fmt.Println(strings.Contains("Germany", "Ger"))    // true 
+    fmt.Println(strings.Contains("Germany", "ger"))    // false 
+    fmt.Println(strings.Contains("Germany", "er"))     // true 
+    fmt.Println() 
+    fmt.Println("Count Example:") 
+    fmt.Println(strings.Count("cheese", "e"))          // 3 
+    fmt.Println() 
+    fmt.Println("EqualFold Examples:") 
+    fmt.Println(strings.EqualFold("Cat", "cAt"))       // true 
+    fmt.Println(strings.EqualFold("India", "Indiana")) // false 
+} 
