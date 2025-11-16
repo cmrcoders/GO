d681afdd7e3116342921e0dca60995e19ed4de19
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:50:59 +0530

    Update main.go

--- 9. Write a GO Program to calculate average using/main.go
+++ 9. Write a GO Program to calculate average using/main.go
@@ -1,18 +1,16 @@
 // 9. Write a GO Program to calculate average using arrays.
-package main
-
-import "fmt"
-
-func main() {
-	var num [100]int
-	var n, sum int
-	fmt.Print("Enter number of elements: ")
-	fmt.Scanln(&n)
-	for i := 0; i < n; i++ {
-		fmt.Printf("Enter number %d: ", i+1)
-		fmt.Scanln(&num[i])
-		sum += num[i]
-	}
-	avg := float64(sum) / float64(n)
-	fmt.Printf("\nAverage of entered %d number(s) is: %.2f\n", n, avg)
-}
+package main 
+import "fmt" 
+func main() { 
+    var num [100]int 
+    var n, sum int 
+    fmt.Print("Enter number of elements: ") 
+    fmt.Scanln(&n) 
+    for i := 0; i < n; i++ { 
+        fmt.Printf("Enter number %d: ", i+1) 
+        fmt.Scanln(&num[i]) 
+        sum += num[i] 
+    } 
+    avg := float64(sum) / float64(n) 
+    fmt.Printf("\nAverage of %d number(s) is: %.2f\n", n, avg) 
+} 
