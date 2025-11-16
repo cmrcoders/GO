4def57c823be2646dcb864c452e4ab678daf7e49
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:47:02 +0530

    Update main.go

--- 5. Write a program in GO language to/main.go
+++ 5. Write a program in GO language to/main.go
@@ -1,19 +1,16 @@
 // 5. Write a GO Program to print Floyd’s Triangle.
-package main
-
-import "fmt"
-
-func main() {
-	var rows int
-	number := 1
-	fmt.Print("Enter number of rows: ")
-	fmt.Scan(&rows)
-	fmt.Println("\nFloyd’s Triangle:")
-	for i := 1; i <= rows; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Printf("%d ", number)
-			number++
-		}
-		fmt.Println()
-	}
+package main 
+import "fmt" 
+func main() { 
+    var rows int 
+    temp := 1 
+    fmt.Print("Enter number of rows: ") 
+    fmt.Scan(&rows) 
+    for i := 1; i <= rows; i++ { 
+        for j := 1; j <= i; j++ { 
+            fmt.Printf("%d ", temp) 
+            temp++ 
+        } 
+        fmt.Println() 
+    } 
 }
