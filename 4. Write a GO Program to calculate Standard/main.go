6e1e157bfacf5cad01f69765ad8236b589ec921e
cmrcoders <cmrcoders@gmail.com>
2025-11-16 22:46:17 +0530

    Update main.go

--- 4. Write a GO Program to calculate Standard/main.go
+++ 4. Write a GO Program to calculate Standard/main.go
@@ -1,25 +1,21 @@
-package main
-
-import (
-	"fmt"
-	"math"
-)
-
-func main() {
-	var n int
-	fmt.Print("Enter number of elements: ")
-	fmt.Scan(&n)
-	nums := make([]float64, n)
-	var sum, mean, sd float64
-	fmt.Println("Enter elements:")
-	for i := 0; i < n; i++ {
-		fmt.Scan(&nums[i])
-		sum += nums[i]
-	}
-	mean = sum / float64(n)
-	for i := 0; i < n; i++ {
-		sd += math.Pow(nums[i]-mean, 2)
-	}
-	sd = math.Sqrt(sd / float64(n))
-	fmt.Printf("\nStandard Deviation = %.2f\n", sd)
-}
+package main  
+import (  
+    "fmt"  
+    "math"  
+)  
+func main() {  
+    var num [10]float64  
+    var sum, mean, sd float64  
+    fmt.Println("Enter 10 numbers:")  
+    for i := 0; i < 10; i++ {  
+        fmt.Printf("Value %d: ", i+1)  
+        fmt.Scan(&num[i])  
+        sum += num[i]  
+    }  
+    mean = sum / 10  
+    for i := 0; i < 10; i++ {  
+        sd += math.Pow(num[i]-mean, 2)  
+    }  
+    sd = math.Sqrt(sd / 10)  
+    fmt.Printf("\nStandard Deviation = %.4f\n", sd)  
+}  
